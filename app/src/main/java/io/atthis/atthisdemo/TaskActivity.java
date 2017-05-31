f783e04f7c1c64c34f0003b4893783ef693887b1
WMXPY <wm@wmpcxpy.com>
2017-05-30 23:02:33 -0500

    fine trouble

--- app/src/main/java/io/atthis/atthisdemo/TaskActivity.java
+++ app/src/main/java/io/atthis/atthisdemo/TaskActivity.java
@@ -7,14 +7,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskActivity extends AppCompatActivity {
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
         setTitle("My Tasks");
+        listView = (ListView) this.findViewById(R.id.mainlist);
         Intent intent = getIntent();
         UserInfo userinfo = new UserInfo();
         userinfo.authority = intent.getStringExtra("authority");
@@ -23,7 +30,14 @@ public class TaskActivity extends AppCompatActivity {
         userinfo.id = intent.getStringExtra("id");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        listView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, getData()));
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long arg) {
+                setTitle(position+"Clicjed");
+            }
+        });
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,6 +45,16 @@ public class TaskActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
             }
         });
+    }
+    private List<String> getData(){
+
+        List<String> data = new ArrayList<String>();
+        data.add("测试数据1");
+        data.add("测试数据2");
+        data.add("测试数据3");
+        data.add("测试数据4");
+
+        return data;
     }
     public class UserInfo{
         public String authority;
